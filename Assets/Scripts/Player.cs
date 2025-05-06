50b7d47234583010002e70da3ccc1100f55b876a
Antony Sajan <antonysajant@gmail.com>
2025-05-06 14:32:05 +0530

    movement and jump added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpspeed = 2.5f;
+    [SerializeField] Rigidbody2D rb;
     void Start()
     {
         
     }
 
     void Update()
+    { 
+          movement();
+    }
+
+    void movement()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocityY = speed;
+        }
     }
 }
